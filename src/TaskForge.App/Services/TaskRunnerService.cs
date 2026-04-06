dab6b8186d07e99b1654f06a1ab71c1c982b1b18
theoahhing <theo.ahhing@outlook.com>
2026-04-06 17:50:01 +1000

    Updated Task Runner.
    Added comments for task runner service class. Need to create simple AppTask and TaskAction prior to Task Runner Service.

---
+++ src/TaskForge.App/Services/TaskRunnerService.cs
@@ -0,0 +1,8 @@
+/*
+    Task Runner Service class will handle executables:
+        - Execute process
+        - Start timer
+        - Execute another process
+        - Stops timer
+        - Kills processes due to timer
+*/
