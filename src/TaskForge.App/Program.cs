98a0f7b663d48fa3fe3665aa825cade93db77004
theoahhing <theo.ahhing@outlook.com>
2026-04-06 16:45:48 +1000

    Re-Structure and Testing
    Re-structured the folders to be in TaskForge.App folder and wrote some code in Program.cs to test the Scanner Service to find executables.

--- src/TaskForge.App/Program.cs
+++ src/TaskForge.App/Program.cs
@@ -4,8 +4,28 @@
 
 using TaskForge.App.Services;
 
-Console.WriteLine("TaskForge starting...");
+Console.WriteLine("=== TaskForge ===");
 
 ExeScannerService exeScannerService = new();
 
-Console.WriteLine("Executable scanner service initialized.");
+Console.WriteLine("Enter directory path to scan: ");
+string? inputPath = Console.ReadLine();
+
+try
+{
+    List<string> executables = exeScannerService.FindExecutables(inputPath ?? "");
+
+    Console.WriteLine($"\nFound {executables.Count} executable(s):\n");
+
+    foreach( string executable in executables)
+    {
+        Console.WriteLine(executable);
+    }
+}
+catch(Exception ex)
+{
+    Console.WriteLine($"\n[ERROR]: {ex.Message}");
+}
+
+Console.WriteLine("\nPress any key to exit...");
+Console.ReadKey();
